48199c96300b4deefb79829cc2d4e90432364a9b
Helen Ngo <h.ngo@dessa.com>
2019-12-16 11:32:49 -0500

    added favicon

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, url_for, request
-
+from flask import Flask, render_template, url_for, request, send_from_directory
+import os
 
 app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
