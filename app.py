31cb9f6eb27d93576dc1eecc2fa27685dc6d5cc7
Helen Ngo <h.ngo@dessa.com>
2019-12-18 09:07:19 -0500

    [HN] back to server side

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ def predict():
 if __name__ == '__main__':
 
     # server side:
-    #app.run_server(host='0.0.0.0', port=5000, debug=False)
+    app.run_server(host='0.0.0.0', port=5000, debug=False)
 
     # local:
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    #app.run(host='0.0.0.0', port=5000, debug=False)
 
